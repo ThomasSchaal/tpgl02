e2fc6021f793df685f3f8aa6fe95b6e566392ac3
maxime.reut@gmail.com <maxime.reut@gmail.com@d68083d3-1b50-e4c5-ed39-74152fcac38b>
2014-11-25 23:56:58 +0000

    Pour récupérer les entrées de l'utilisateur comme le nom,prénom ou chemin des fichiers

---
+++ trunk/Projet/recuperationEntreeClavier.js
@@ -0,0 +1,14 @@
+function ask(question, format, callback) {
+    var stdin = process.stdin, stdout = process.stdout;
+    stdin.resume();
+    stdout.write(question + ": ");
+    stdin.once('data', function(data) {
+        data = data.toString().trim();
+        if (format.test(data)) {
+            callback(data);
+        } else {
+            stdout.write("Format incorect, celui ci doit être sous la forme: " + format + "\n");
+            ask(question, format, callback);
+        }
+    });
+}
